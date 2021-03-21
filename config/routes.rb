9d162a70d5f8f1f24f87259d695d5864b47da341
Bohdan Oodovskyi <lsdovscky@gmail.com>
2021-03-21 22:46:14 +0200

    add CRUD endpoinds for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      resources :users, only: %I[show create] do
+        collection do
+          get :profile
+          put :profile, action: :update
+          delete :profile, action: :destroy
+        end
+      end
+
       resources :sessions, only: %I[create] do
         collection do
           delete :logout, action: :destroy
